a9faab4fa60ee02130748d93f803b738d943c103
Joel Friedman <asher.friedman@gmail.com>
2012-08-27 14:25:56 -0500

    loading AMPQ url through dotenv

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ gem 'haml'
 gem 'sass'
 gem 'mostash'
 gem 'bunny'
+gem 'dotenv', :groups => [:development, :test]
 gem 'sinatra_more', :require => false
 gem 'sinatra-flash', :require => 'sinatra/flash'
--- web_runner.rb
+++ web_runner.rb
@@ -14,10 +14,16 @@ end
 CONFIG = Mostash.new
 
 CONFIG.env = ENV['RAB_PUB_ENV']
+
+unless CONFIG.env == 'production'
+  require 'dotenv'
+  Dotenv.load
+end
+
 CONFIG.rabbit = {
   :enabled => ENV['WITHOUT_RABBIT'] || true,
   :broker => {
-    :url => ENV['CLOUDAMQP_URL'] || 'amqp://app6795846_heroku.com:ggZ9ywmvRTbPD-NfOMIcCr6KqywHviYy@tiger.cloudamqp.com/app6795846_heroku.com'
+    :url => ENV['CLOUDAMQP_URL'] || (raise Exception.new('CLOUDAMQP_URL must be set as env variable'))
   },
   :outbound => {
     :exchange => {
